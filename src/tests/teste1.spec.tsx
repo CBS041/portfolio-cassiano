2dca3e2b9557d04e15d107a42683119d83115a72
CBS041 <cassianobrito446@gmail.com>
2026-09-07 04:35:04 -0300

    chore: Removendo temporariamente arquivos de teste

--- src/tests/teste1.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@/lib/test-utils'
-
-describe('Teste', () => {
-  it('deve passar', () => {
-    render(<div>Teste</div>)
-
-    expect(screen.getByText('Teste')).toBeInTheDocument()
-  })
-})
